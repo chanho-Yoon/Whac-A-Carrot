2933d838a7b0c99c228514c89bcadd406261d77f
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-10-30 15:04:31 +0900

    타입 보장을 위해 Reason 오브젝트 생성 및 기존 문자열로 인자를 전달하던걸 Reason.win lose cancel 로 전달
    게임 난이도 화면에 출력
    게임 레벨 기능 추가
    
    랜덤한 위치로 움직이는 벌레 moveBug() 함수 구현
    2초마다 한 번씩 움직이도록 randomMoveBug() 함수 구현

--- src/game.js
+++ src/game.js
@@ -1,11 +1,19 @@
 'use strict';
 
-import Field from './field.js';
+import Field, { ItemType } from './field.js';
 import * as sound from './sound.js';
 import PopUp from './popup.js';
+import { BUG_COUNT, CARROT_COUNT } from './main.js';
+
+
+export const Reason = Object.freeze({
+	win   : 'win',
+	lose  : 'lose',
+	cancel: 'cancel'
+});
 
 // Builder Pattern
-const GameBuilder = class GameBuilder {
+export const GameBuilder = class GameBuilder {
 	WithCarrotSize( num ) {
 		this.carrotSize = num;
 		return this;
@@ -42,7 +50,9 @@ class Game {
 		this.gameCount = gameCount;
 		this.carrotCount = carrotCount;
 		this.bugCount = bugCount;
-
+		this.level = 1;
+		// 레벨 표시
+		this.gameLevel = document.querySelector('.game__level');
 		// 게임 버튼, 타이머, 스코어
 		this.gameBtn = document.querySelector('.game__button');
 		this.gameTimer = document.querySelector('.timer');
@@ -53,7 +63,8 @@ class Game {
 		this.score = 9; // 게임 스코어 저장하는 변수
 		this.timer = undefined; // setInterval 저장할 변수 ( 종료하거나 성공시 함수 종료 위해)
 		this.countDownSecond = undefined; // 제한시간
-
+		// 움직이는 벌레 타이머
+		this.bugTimer = undefined;
 		// 필드 모듈
 		this.gameField = new Field(carrotSize, carrotCount, bugCount);
 		this.gameField.setClickListener(this.onItemClick);
@@ -68,69 +79,79 @@ class Game {
 
 	// gameField에 랜덤으로 이미지 추가
 	initGame() {
-		this.gameField.init();
+		this.gameField.init(this.carrotCount, this.bugCount);
 	};
 
 	// 게임 시작 여부에 따라 시작 및 정지
 	gameStart = () => {
 		if (this.started === true) {
-			this.stopGame();
+			this.stopGame(Reason.cancel);
 		} else {
 			this.startGame();
 		}
 		this.started = !this.started;
 	};
 
+	// 이미지 클릭 처리 ( 점수 카운트 감소 및 성공 여부 출력 )
+	onItemClick = ( item ) => {
+		if (item === ItemType.carrot) {
+			this.score++;
+			this.updateScoreBoard();
+			if (this.score === this.carrotCount) {
+				this.stopGame(Reason.win);
+				this.started = false;
+			}
+		} else if (item === ItemType.bug) {
+			this.stopGame(Reason.lose);
+			this.started = false;
+		}
+	};
+
+	// 게임 난이도 화면에 출력
+	levelUp( reason ) {
+		switch (reason) {
+			case Reason.win:
+				// 게임 이겼을 시에 level +1
+				this.level++;
+				this.gameLevel.innerText = `level : ${this.level}`;
+				// 난이도를 위해 carrot,bug count +1
+				this.carrotCount++;
+				this.bugCount++;
+				break;
+			case Reason.lose:
+				this.level = 1;
+				this.gameLevel.innerText = `level : ${this.level}`;
+				this.carrotCount = CARROT_COUNT;
+				this.bugCount = BUG_COUNT;
+				break;
+		}
+	}
+
 	// 게임 시작
 	startGame() {
 		sound.playMainSound();
 		this.gameFinishBanner.popUpHide();
-		this.timeAndScoreShow();
 		this.initGame();
-		this.CountDownTime();
+		this.timeAndScoreShow();
+		this.countDownTime();
+		this.randomMoveBug();
 	}
 
 	// 게임 정지
-	stopGame() {
+	stopGame( reason ) {
 		sound.playStopMainSound();
 		this.gameField.removeImg();
 		this.timeAndScoreHide();
-		this.onGameStop && this.onGameStop('cancel');
+		this.onGameStop && this.onGameStop(reason);
 		clearInterval(this.timer);
-	}
-
-	// 이미지 클릭 처리 ( 점수 카운트 감소 및 성공 여부 출력 )
-	onItemClick = ( item ) => {
-		if (item === 'carrot') {
-			this.score++;
-			this.updateScoreBoard();
-			if (this.score === this.carrotCount) {
-				this.finishGame(true);
-			}
-		} else if (item === 'bug') {
-			this.finishGame(false);
-		}
-	};
-
-	// 게임 성공, 실패에 따른 함수 실행 및 메시지 전달
-	finishGame( win ) {
-		if (win) {
-			sound.playWinSound();
-			this.gameStart();
-			this.onGameStop && this.onGameStop('win');
-			clearInterval(this.timer);
-		} else {
-			sound.playBugSound();
-			this.gameStart();
-			this.onGameStop && this.onGameStop('lose');
-			clearInterval(this.timer);
-		}
+		clearInterval(this.bugTimer);
 	}
 
 	// 게임 타임, 스코어 가리는 함수 및 스코어 초기화
 	timeAndScoreHide() {
 		this.gameTimer.classList.add('timer__hide');
 		this.gameScore.classList.add('score__hide');
+		this.gameBtn.classList.add('game__button-hide');
 		this.gameBtn.innerHTML = `<i class="fas fa-play"></i>`;
 	}
 
@@ -138,6 +159,7 @@ class Game {
 	timeAndScoreShow() {
 		this.gameTimer.innerHTML = `0:${this.gameCount}`;
 		this.gameScore.innerHTML = `${this.carrotCount}`;
+		this.gameBtn.classList.remove('game__button-hide');
 		this.gameTimer.classList.remove('timer__hide');
 		this.gameScore.classList.remove('score__hide');
 		this.gameBtn.innerHTML = `<i class="fas fa-times"></i>`;
@@ -147,9 +169,23 @@ class Game {
 	updateScoreBoard() {
 		this.gameScore.innerText = this.carrotCount - this.score;
 	}
+	// 랜덤한 위치로 움직이는 벌레
+	moveBug() {
+		const x1 = 0;
+		const y1 = 0;
+		const x2 = this.gameField.fieldRect.width - this.carrotSize;
+		const y2 = this.gameField.fieldRect.height - this.carrotSize;
+
+		for(let i=0; i < this.gameField.bugArray.length; i++) {
+			let x = this.gameField.randomNumber(x1, x2);
+			const y = this.gameField.randomNumber(y1, y2);
+			this.gameField.bugArray[i].style.left = `${x}px`;
+			this.gameField.bugArray[i].style.top = `${y}px`;
+		}
+	}
 
 	// 시간제한 카운트 다운 시작
-	CountDownTime() {
+	countDownTime() {
 		this.score = 0;
 		this.countDownSecond = this.gameCount;
 		this.timer = setInterval(() => {
@@ -157,16 +193,18 @@ class Game {
 			this.countDownSecond -= 1;
 			// 게임 실패 ( 시간 초과 )
 			if (this.countDownSecond < 0) {
-				this.finishGame(false);
+				this.stopGame(Reason.lose);
+				this.started = false;
 			}
 			// 남은 시간 4초 이내 경고 사운드
 			if (this.countDownSecond < 5) {
 				sound.playAlertSound();
 			}
-
 		}, 1000);
-
 	};
+	randomMoveBug() {
+		this.bugTimer = setInterval(() => {
+			this.moveBug()
+		},2000)
+	}
 }
-
-export default GameBuilder;
