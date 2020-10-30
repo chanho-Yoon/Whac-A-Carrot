c8cf874da73b71b8f1b3cbd3bb50081eab58422d
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-10-30 15:01:05 +0900

    타입 보장을 위해 ItemType 오브젝트 추가 및 기존에 문자열로 전달되던 것을 ItemType.carrot, ItemType.bug 전달
    gameField에 랜덤으로 이미지를 추가하는 init() 함수에 파라미터로 carrotCount,bugCount를 받음 게임 레벨이 상승함에 따라 carrot과 bug의 count가 올라가기 때문
    
    그리고 각각의 bug 오브젝트를 움직이기 위해 bugArray 배열에 bug 오브젝트들을 담음

--- src/field.js
+++ src/field.js
@@ -2,13 +2,18 @@
 
 import * as sound from './sound.js';
 
+export const ItemType = Object.freeze({
+	carrot: 'carrot',
+	bug   : 'bug'
+});
+
 const Field = class Field {
 	// 게임 구역
 	constructor( carrotSize, carrotCount, bugCount ) {
 		this.carrotSize = carrotSize;
 		this.carrotCount = carrotCount;
+		this.bugArray = []; // bug 움직이기 위해서 각각의 bug객체 저장할 배열
 		this.bugCount = bugCount;
-
 		this.field = document.querySelector('.game__field');
 		this.fieldRect = this.field.getBoundingClientRect();
 		this.field.addEventListener('click', this.onClick);
@@ -23,10 +28,10 @@ const Field = class Field {
 		if (imgName === 'carrot') {
 			event.target.remove();
 			sound.playCarrotSound();
-			this.onItemClick && this.onItemClick('carrot');
+			this.onItemClick && this.onItemClick(ItemType.carrot);
 		} else if (imgName === 'bug') {
 			sound.playBugSound();
-			this.onItemClick && this.onItemClick('bug');
+			this.onItemClick && this.onItemClick(ItemType.bug);
 		}
 	};
 
@@ -36,12 +41,15 @@ const Field = class Field {
 	}
 
 // gameField에 랜덤으로 이미지 추가
-	init() {
+	init(carrotCount, bugCount) {
 		this.removeImg();
-		this.addItem('carrot', this.carrotCount, 'img/carrot.png');
-		this.addItem('bug', this.bugCount, 'img/bug.png');
+		this.addItem(ItemType.carrot, carrotCount, 'img/carrot.png');
+		this.addItem(ItemType.bug, bugCount, 'img/bug.png');
 	};
-
+// 각각의 이미지를 gameField에 위치시킬 x,y값 랜덤 구하는 함수
+	randomNumber( minValue, maxValue ) {
+		return Math.floor(Math.random() * ( maxValue - minValue ) + minValue);
+	}
 // carrot,bug 이미지를 count만큼 gameField에 배치
 	addItem( className, count, imgPath ) {
 		const x1 = 0;
@@ -53,20 +61,20 @@ const Field = class Field {
 			item.setAttribute('class', className);
 			item.setAttribute('src', imgPath);
 			item.style.position = 'absolute';
-			const x = randomNumber(x1, x2);
-			const y = randomNumber(y1, y2);
+			const x = this.randomNumber(x1, x2);
+			const y = this.randomNumber(y1, y2);
 
 			item.style.left = `${x}px`;
 			item.style.top = `${y}px`;
 			this.field.appendChild(item);
+
+			if(item.className === ItemType.bug) {
+				this.bugArray.push(item);
+			}
 		}
 	};
 };
 
-// 각각의 이미지를 gameField에 위치시킬 x,y값 랜덤 구하는 함수
-function randomNumber( minValue, maxValue ) {
-	return Math.floor(Math.random() * ( maxValue - minValue ) + minValue);
-}
 
 
 export default Field;
