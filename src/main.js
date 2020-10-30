297e8345ff5fcb2182a629ac2cc1966908d8cc7a
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-10-30 15:08:15 +0900

    콜백함수인 setGameStopListener에서 결과 값 reason을 받아와 case 조건문에서 message 전달 및 사운드 출력 각각의 조건문에 해당하는 곳에서 game.levelUp(parameter) 파라미터로 Reason.win 또는 Reason.lose 전달해서 레벨업 또는 레벨 초기화 시킴

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 'use strict';
 
 import PopUp from './popup.js';
-import GameBuilder from './game.js';
+import { GameBuilder, Reason } from './game.js';
+import * as sound from './sound.js';
 
 const CARROT_SIZE = 80;
-const CARROT_COUNT = 20;
-const BUG_COUNT = 20;
+export const CARROT_COUNT = 5;
+export const BUG_COUNT = 5;
 const GAME_COUNT = 15;
-
 
 // 게임 모듈
 const game = new GameBuilder()
@@ -17,27 +17,34 @@ const game = new GameBuilder()
 	.WithBugCount(BUG_COUNT)
 	.build();
 
-
 // 팝업 모듈
 const gameFinishBanner = new PopUp();
 gameFinishBanner.setClickListener(() => {
 	game.gameStart();
 });
 
-// 콜백으로 값을 받아 pop창에 나타낼 메시지 함수 호출
-game.setGameStopListener(( message ) => {
-	switch (message) {
-		case 'win':
-			gameFinishBanner.gamePopUpMessgae('성공👍🏻');
+// 콜백으로 값을 받아 popUp창에 나타낼 메시지 함수 호출
+game.setGameStopListener(( reason ) => {
+	let message;
+	switch (reason) {
+		case Reason.win:
+			message = '성공👍🏻';
+			sound.playWinSound();
+			game.levelUp(Reason.win); // 게임 이겼을 시 레벨업
 			break;
-		case 'cancel':
-			gameFinishBanner.gamePopUpMessgae('취소❌');
+		case Reason.cancel:
+			message = '취소❌';
+			sound.playAlertSound();
+			game.levelUp(Reason.lose) // 게임 취소시 레벨 초기화
 			break;
-		case 'lose':
-			gameFinishBanner.gamePopUpMessgae('실패😂');
+		case Reason.lose:
+			message = '실패😂';
+			sound.playBugSound();
+			game.levelUp(Reason.lose) // 게임 실패시 레벨 초기화
 			break;
 		default :
 			console.log('error');
 			break;
 	}
+	gameFinishBanner.gamePopUpMessgae(message);
 });
